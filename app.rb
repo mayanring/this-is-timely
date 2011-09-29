bdfce28bf4ff8e1d86b26a462569793924dcbaac
Ryan Ming <ryan.ming@gmail.com>
2011-09-28 23:03:09 -0400

    Convert parameterized time into total time in seconds.

--- app.rb
+++ app.rb
@@ -5,16 +5,28 @@ Bundler.require
 
 set :haml, :format => :html5
 
+MINUTE = 60
+HOUR = 3600
+DAY = 86400
+
+helpers do
+  def total_time_in_seconds(days, hours, minutes, seconds)
+    seconds + minutes*MINUTE + hours*HOUR + days*DAY
+  end
+end
+
 get '/:time' do
   days = params[:time].match(/(\d+\s*)d/) { |m| m.captures[0] }
   hours = params[:time].match(/(\d+\s*)h/) { |m| m.captures[0] }
   minutes = params[:time].match(/(\d+\s*)m/) { |m| m.captures[0] }
   seconds = params[:time].match(/(\d+\s*)s/) { |m| m.captures[0] }
 
-  if !days && !hours && !minutes && !seconds
+  @time = total_time_in_seconds(days.to_i, hours.to_i, minutes.to_i, seconds.to_i)
+
+  if @time == 0
     "No time was found!"
   else
-    "#{days} days, #{hours} hours, #{minutes} minutes, #{seconds} seconds"
+    "#{@time}"
   end
 end
 
