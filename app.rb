aec640f8224a46da0dd43f6fd46c52b02f28333e
Ryan Ming <ryan.ming@gmail.com>
2011-09-28 21:59:06 -0400

    Explicitly redirect when time parameter is passed via traditional query parameter.

--- app.rb
+++ app.rb
@@ -1,8 +1,18 @@
 require 'bundler'
+require 'cgi'
+
 Bundler.require
 
 set :haml, :format => :html5
 
+get '/:time' do
+	
+end
+
 get '/' do
-    haml :index
+	if !params[:time].nil? && !params[:time].empty?
+		redirect to("/#{CGI.escape(params[:time])}")	
+	end
+
+	haml :index
 end
