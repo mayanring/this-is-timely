cc784ef49e86a6db7889bc41e5000eae41205ad2
Ryan Ming <ryan.ming@gmail.com>
2012-02-05 23:51:53 +0000

    Remove unnecessary boilerplate, restyle landing using fittext.

---
+++ public/js/timely.js
@@ -0,0 +1,11 @@
+(function($) {
+  $(document).ready(function() {
+    // var timer_config = $('.timer').data();
+    // $('.timer').countdown({
+    //   until: timer_config.timer,
+    //   format: 'dhmS'
+    // });
+
+    $('#timely').fitText(0.3);
+  });
+})(jQuery);
