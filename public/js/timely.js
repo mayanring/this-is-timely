f3ee5a2552b00cfe29832a60ba043b839fd7f3d7
Ryan Ming <ryan.ming@gmail.com>
2012-02-06 00:38:32 +0000

    Some work on the timer page.

--- public/js/timely.js
+++ public/js/timely.js
@@ -1,11 +1,16 @@
 (function($) {
   $(document).ready(function() {
-    // var timer_config = $('.timer').data();
-    // $('.timer').countdown({
-    //   until: timer_config.timer,
-    //   format: 'dhmS'
-    // });
+
+    if ($('.timer').length > 0) {
+      var timer_config = $('.timer').data();
+      $('.timer').countdown({
+        until: timer_config.timer,
+        format: 'dhms',
+        layout: '<div class="time_remaining">{d<}<div class="days">{dn} days</div>{d>}{h<}<div class="hours">{hn} hours</div>{h>}{m<}<div class="minutes">{mn} minutes</div>{m>}{s<}<div class="seconds">{sn} seconds</div>{s>}</div>'
+      });
+    }
 
     $('#timely').fitText(0.3);
+    $('.timer').fitText(0.58);
   });
 })(jQuery);
