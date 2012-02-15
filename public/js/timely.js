32003a4c52861530f6114aa9df99d7c0e1700cf6
Ryan Ming <ryan.ming@gmail.com>
2012-02-15 00:50:07 +0000

    Added ASCII art. Aw yeah.

--- public/js/timely.js
+++ public/js/timely.js
@@ -7,7 +7,10 @@
         until: timer_config.timer,
         format: 'dhms',
         layout: '<div class="time_remaining">{d<}<div class="days">{dn} {dl}</div>{d>}{h<}<div class="hours">{hn} {hl}</div>{h>}{m<}<div class="minutes">{mn} {ml}</div>{m>}{s<}<div class="seconds">{snn} {sl}</div>{s>}</div>',
-        expiryText: "time&#8217;s up"
+        expiryText: "time&#8217;s up",
+        onExpiry: function() {
+          alert("Time's up!");
+        }
       });
     }
 
